10ed1d140d2ee639502aecd2bce4fa5f67e1856a
Domagoj Margan <margan.domagoj@gmail.com>
2016-08-05 22:39:39 +0100

    Added xmonad.hs

---
+++ xmonad.hs
@@ -0,0 +1,28 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+ 
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+      , isFullscreen --> doFullFloat
+    ]
+
+myFocusedBorderColor = "#93E0E3"
+
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> myManageHook
+                        <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+			, ppTitle = xmobarColor "#60B48A" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+	, focusedBorderColor = myFocusedBorderColor
+        }
